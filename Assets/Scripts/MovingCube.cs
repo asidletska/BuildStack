4bc071ad579781d73c20990a4f2ea2761b3d9e13
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-08-06 18:31:58 +0300

    Moving Cube Script

--- Assets/Scripts/MovingCube.cs
+++ Assets/Scripts/MovingCube.cs
@@ -1,20 +1,56 @@
+using System;
 using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
     public static MovingCube CurrentCube {  get; private set; }
+    public static MovingCube LastCube { get; private set; }
 
     [SerializeField]
     private float moveSpeed = 1f;
 
     private void OnEnable()
     {
+        if (LastCube == null)
+        {
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+        }
         CurrentCube = this;
     }
     internal void Stop()
     {
         moveSpeed = 0f;
+        float hangover = transform.position.z - LastCube.transform.position.z;
+        float direction = hangover > 0 ? 1f : -1f;
+        SplitCubeOnZ(hangover, direction);
+        //Debug.Log(hangover);
     }
+
+    private void SplitCubeOnZ(float hangover, float direction)
+    {
+        float newSize = LastCube.transform.lossyScale.z - Mathf.Abs(hangover); 
+        float fallingBlockSize = transform.lossyScale.z - newSize;
+
+        float newZPosition = LastCube.transform.position.z + (hangover / 2);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newSize);
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
+
+        float cubeEdge = transform.position.z + (newSize / 2 * direction);
+        float fallingBlockZPosition = cubeEdge + fallingBlockSize / 2f * direction;
+
+        SpawnDropCube(fallingBlockZPosition, fallingBlockSize);
+    }
+
+    private void SpawnDropCube(float fallingBlockZPosition, float fallingBlockSize)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, fallingBlockSize);
+        cube.transform.position = new Vector3(transform.position.x, transform.position.y, fallingBlockZPosition);
+
+        cube.AddComponent<Rigidbody>();
+        Destroy(cube.gameObject, 1f);
+    }
+
     private void Update()
     {
         transform.position += transform.forward * Time.deltaTime * moveSpeed;
